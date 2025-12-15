5d65c12a9f6f6ddf3f8508dec8b0cc7876aaf926
vxcxntv <contato.mtvicente@gmail.com>
2025-12-15 16:29:51 -0300

    adaptação para subir aplicação

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+
 // Defina a URL base da sua API (ajuste se sua porta for diferente, ex: 3000 ou 8080)
 export const api = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: baseURL,
 });
 
 // Interceptor para adicionar o Token automaticamente (preparando para o futuro)
